756387c161d2c2c195964743c299c51573ad0672
AlejaB-droid <alejandra.barrios.1900@gmail.com>
2021-05-09 21:15:42 -0500

    second commit

--- assets/js/reto.js
+++ assets/js/reto.js
@@ -1,6 +1,7 @@
 // Variables h3
 let letras = document.getElementById("letras");
 let numeros = document.getElementById("numeros");
+
 // Variables botones
 let btnA1 = document.getElementById("btnA1");
 let btnB2 = document.getElementById("btnB2");
@@ -12,55 +13,191 @@ let btnG7 = document.getElementById("btnG7");
 let btnH8 = document.getElementById("btnH8");
 let btnI9 = document.getElementById("btnI9");
 let btnJ0 = document.getElementById("btnJ0");
+
 //Variables temp
 let letrasTemp = "";
 let numerosTemp = "";
+
+//Contadores
 let contLetras = 0;
 let contNumeros = 0;
 
 //Funciones
 const btnA1f = () => {
     if (contLetras == "0") {
-        letras.innerHTML = "A";
-        letrasTemp = "A";
+        letrasTemp += "A";
+        letras.innerHTML = letrasTemp;
         contLetras = contLetras + 1;
     } else {
-        alert("Digite todas las letras en orden alfabético");
-    }
-    if (contNumeros == "0" && contLetras == "10"){
-        numeros.innerHTML = "1";
-        numerosTemp = "1";
-        contNumeros = contNumeros + 1;
-    }
-
+        if (contNumeros == "1" && contLetras == "10"){
+            numerosTemp += "1";
+            numeros.innerHTML = numerosTemp;
+            contNumeros = contNumeros + 1;
+        }else {
+            check();
+        };
+    };
 };
+
 const btnB2f = () => {
     if (contLetras == "1") {
-        letras.innerHTML = letras + "B";
-        letrasTemp = "B";
+        letrasTemp += "B";
+        letras.innerHTML = letrasTemp;
         contLetras = contLetras + 1;
     } else {
-        alert("Digite las letras en orden alfabético");
-    }
-    if (contNumeros == "1"){
-        numeros.innerHTML = "2";
-        numerosTemp = "2";
-        contNumeros = contNumeros + 1;
-    }
-
+        if (contNumeros == "2" && contLetras == "10"){
+            numerosTemp += "2";
+            numeros.innerHTML = numerosTemp;
+            contNumeros = contNumeros + 1;
+        }else{
+            check();
+        };
+    };
 };
-// const llenarLetras = () => {
-//     if(){
 
-//     }
-// };
-// const checkLetras = () => {
-//     cont = cont + 1;
-//     return cont();
-// };
-const checkNumeros = () => {
-
+const btnC3f = () => {
+    if (contLetras == "2") {
+        letrasTemp += "C";
+        letras.innerHTML = letrasTemp;
+        contLetras = contLetras + 1;
+    } else {
+        if (contNumeros == "3" && contLetras == "10"){
+            numerosTemp += "3";
+            numeros.innerHTML = numerosTemp;
+            contNumeros = contNumeros + 1;
+        }else{
+            check();
+        };
+    };
 };
+
+const btnD4f = () => {
+    if (contLetras == "3") {
+        letrasTemp += "D";
+        letras.innerHTML = letrasTemp;
+        contLetras = contLetras + 1;
+    } else {
+        if (contNumeros == "4" && contLetras == "10"){
+            numerosTemp += "4";
+            numeros.innerHTML = numerosTemp;
+            contNumeros = contNumeros + 1;
+        }else{
+            check();
+        };
+    };
+};
+
+const btnE5f = () => {
+    if (contLetras == "4") {
+        letrasTemp += "E";
+        letras.innerHTML = letrasTemp;
+        contLetras = contLetras + 1;
+    } else {
+        if (contNumeros == "5" && contLetras == "10"){
+            numerosTemp += "5";
+            numeros.innerHTML = numerosTemp;
+            contNumeros = contNumeros + 1;
+        }else{
+            check();
+        };
+    };
+};
+
+const btnF6f = () => {
+    if (contLetras == "5") {
+        letrasTemp += "F";
+        letras.innerHTML = letrasTemp;
+        contLetras = contLetras + 1;
+    } else {
+        if (contNumeros == "6" && contLetras == "10"){
+            numerosTemp += "6";
+            numeros.innerHTML = numerosTemp;
+            contNumeros = contNumeros + 1;
+        }else{
+            check();
+        };
+    };
+};
+
+const btnG7f = () => {
+    if (contLetras == "6") {
+        letrasTemp += "G";
+        letras.innerHTML = letrasTemp;
+        contLetras = contLetras + 1;
+    } else {
+        if (contNumeros == "7" && contLetras == "10"){
+            numerosTemp += "7";
+            numeros.innerHTML = numerosTemp;
+            contNumeros = contNumeros + 1;
+        }else{
+            check();
+        };
+    };
+};
+
+const btnH8f = () => {
+    if (contLetras == "7") {
+        letrasTemp += "H";
+        letras.innerHTML = letrasTemp;
+        contLetras = contLetras + 1;
+    } else {
+        if (contNumeros == "8" && contLetras == "10"){
+            numerosTemp += "8";
+            numeros.innerHTML = numerosTemp;
+            contNumeros = contNumeros + 1;
+        }else{
+            check();
+        };
+    };
+};
+
+const btnI9f = () => {
+    if (contLetras == "8") {
+        letrasTemp += "I";
+        letras.innerHTML = letrasTemp;
+        contLetras = contLetras + 1;
+    } else {
+        if (contNumeros == "9" && contLetras == "10"){
+            numerosTemp += "9";
+            numeros.innerHTML = numerosTemp;
+            contNumeros = contNumeros + 1;
+        }else{
+            check();
+        };
+    };
+};
+
+const btnJ0f = () => {
+    if (contLetras == "9") {
+        letrasTemp += "J";
+        letras.innerHTML = letrasTemp;
+        contLetras = contLetras + 1;
+    } else {
+        if (contNumeros == "0" && contLetras == "10"){
+            numerosTemp += "0";
+            numeros.innerHTML = numerosTemp;
+            contNumeros = contNumeros + 1;
+        }else{
+            check();
+        };
+    };
+};
+
+const check = () =>{
+    if (contLetras == "10" && contNumeros  == "10") {
+        alert("Ya digitó todas las letras y números correctamente :D");
+        }
+        else{
+            if (contNumeros != "10" && contLetras == "10"){
+                alert("Digite los números de forma ascendente y sin repetir");
+            }else{
+                if (contLetras != "10") {
+                    alert("Digite las letras en orden alfabético y sin repetir");
+            };
+        };
+    };
+};
+
 
 // Onclick
 btnA1.onclick = () => {
